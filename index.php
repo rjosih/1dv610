38258d974c7e6fbab248f15469ee8907c84b0cfb
ericbrena <eb223fz@student.lnu.se>
2017-09-04 11:16:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-
+<?php
+$sourceFile = file_get_contents("source.php") or die ("failed");
+$sourceToString = htmlspecialchars($sourceFile);$longestCode = 0;
+$lengthOfCode = 0;
+for($i = 0; $i < strlen($sourceToString); $i++){    
+  $lengthOfCode += 1;    
+  if($sourceToString[$i] == "\n"){        
+    if($longestCode < $lengthOfCode){            
+      $longestCode = $lengthOfCode;        
+    }        
+    $lengthOfCode = 0;    
+  }
+}
+echo "<html>
+<body>    
+$sourceToString    
+$longestCode
+</body>
+</html>";
